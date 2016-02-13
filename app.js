6bba2bec979a9cde5a041129aa9425e4bb628c9a
ethanhoneycutt <ethan.honeycutt@yahoo.com>
2016-02-12 20:48:02 -0600

    folder moving, port 80

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var routes = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views/respond_15_template_2446/'));
+app.set('views', path.join(__dirname, 'views/'));
 app.engine('.html', require('ejs').renderFile);
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -18,7 +18,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'views/respond_15_template_2446')));
+app.use(express.static(path.join(__dirname, 'views/')));
 
 app.use('/', routes);
 
